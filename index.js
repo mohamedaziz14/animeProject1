73c9fbb02ef408cb100298ec4ecf6a73521c5128
mohamedaziz14 <jo56501283@gmail.com>
2023-11-20 16:46:03 +0100

    50%

---
+++ index.js
@@ -0,0 +1,45 @@
+var likers=0;
+ $(".uplike").click(function(){
+likers++;
+    $(".like").text(counter);
+})
+$(".downlike").click(function(){
+    likers--;    
+    $(".like").text(counter);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
