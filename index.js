96a7ccad32fd39cfe32e843879ba5a1e3711d344
mohamedaziz14 <jo56501283@gmail.com>
2023-11-21 09:06:56 +0100

    init

--- index.js
+++ index.js
@@ -1,45 +1,103 @@
-var likers=0;
- $(".uplike").click(function(){
-likers++;
-    $(".like").text(counter);
-})
-$(".downlike").click(function(){
-    likers--;    
-    $(".like").text(counter);
-  });
+$(document).ready(function() {
+    var likeCount = 0;
+    var dislikeCount = 0;
 
+    // Handle the click event for the like button
+    $(".likeButton1").click(function() {
+        console.log("hello fawez ");
+        likeCount++;
+        $(".likeCount1").text(likeCount);
+    });
 
+    // Handle the click event for the dislike button
+    $(".dislikeButton").click(function() {
+        dislikeCount++;
+        $(".dislikeCount").text(dislikeCount);
+    });
+});
 
+$(document).ready(function() {
+    var likeCount = 0;
+    var dislikeCount = 0;
+$(".likeButton2").click(function() {
+    console.log("hello fawez ");
+    likeCount++;
+    $(".likeCount2").text(likeCount);
+});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+// Handle the click event for the dislike button
+$(".dislikeButton").click(function() {
+    dislikeCount++;
+    $(".dislikeCount").text(dislikeCount);
+});
+$(document).ready(function() {
+    var likeCount = 0;
+    var dislikeCount = 0;
+$(".likeButton3").click(function() {
+    console.log("hello fawez ");
+    likeCount++;
+    $(".likeCount3").text(likeCount);
+});
+});
+// Handle the click event for the dislike button
+$(".dislikeButton").click(function() {
+    dislikeCount++;
+    $(".dislikeCount").text(dislikeCount);
+});
+$(document).ready(function() {
+    var likeCount = 0;
+    var dislikeCount = 0;
+$(".likeButton4").click(function() {
+    console.log("hello fawez ");
+    likeCount++;
+    $(".likeCount4").text(likeCount);
+});
+});
+// Handle the click event for the dislike button
+$(".dislikeButton").click(function() {
+    dislikeCount++;
+    $(".dislikeCount").text(dislikeCount);
+});
+$(document).ready(function() {
+    var likeCount = 0;
+    var dislikeCount = 0;
+$(".likeButton5").click(function() {
+    console.log("hello fawez ");
+    likeCount++;
+    $(".likeCount5").text(likeCount);
+});
+});
+// Handle the click event for the dislike button
+$(".dislikeButton").click(function() {
+    dislikeCount++;
+    $(".dislikeCount").text(dislikeCount);
+});
+$(document).ready(function() {
+    var likeCount = 0;
+    var dislikeCount = 0;
+$(".likeButton6").click(function() {
+    console.log("hello fawez ");
+    likeCount++;
+    $(".likeCount6").text(likeCount);
+});
+});
+// Handle the click event for the dislike button
+$(".dislikeButton").click(function() {
+    dislikeCount++;
+    $(".dislikeCount").text(dislikeCount);
+});
+$(document).ready(function() {
+    var likeCount = 0;
+    var dislikeCount = 0;
+$(".likeButton7").click(function() {
+    console.log("hello fawez ");
+    likeCount++;
+    $(".likeCount7").text(likeCount);
+});
+});
+// Handle the click event for the dislike button
+$(".dislikeButton").click(function() {
+    dislikeCount++;
+    $(".dislikeCount").text(dislikeCount);
+});
